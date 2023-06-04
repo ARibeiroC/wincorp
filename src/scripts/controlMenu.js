78fe0b26e5443d95ce693b4fff3ada112b257e74
Anderson F. Ribeiro Campos <andersonspl@hotmail.com>
2023-06-04 13:17:38 -0300

    Deploy 1.1.1
    Alteração da sessão Projetos para Cursos.
    Algumas melhorias no código de Menu.

--- src/scripts/controlMenu.js
+++ src/scripts/controlMenu.js
@@ -14,15 +14,13 @@ const menuContact = document.querySelector('#menuContact')
 // Escutando Clicks
 function listens(){
     iconMenu.addEventListener('click', activeMenu)
-    // checkbox.addEventListener("click", controlMenu)
-    // iconMenu.addEventListener("click", controlMenu)
     menuHome.addEventListener("click", activeMenu)
     menuAbout.addEventListener("click", activeMenu)
     menuWork.addEventListener("click", activeMenu)
     menuWorkUs.addEventListener("click", activeMenu)
     menuContact.addEventListener("click", activeMenu)
     
-    // document.addEventListener('scroll', showAndHiddenButtonBackToTop)
+    document.addEventListener('scroll', showAndHiddenButtonBackToTop)
 }
 
 function activeMenu(){
@@ -30,23 +28,14 @@ function activeMenu(){
 }
 
 
-// function showAndHiddenButtonBackToTop() {
-//     if (document.documentElement.scrollTop > 10) {
-//         backTop.style.top = '90vh'
-//     } else {
-//         backTop.style.top = '-10vh'
-//     }
-// }
+function showAndHiddenButtonBackToTop() {
+    if (document.documentElement.scrollTop > 10) {
+        backTop.style.top = '90vh'
+    } else {
+        backTop.style.top = '-10vh'
+    }
+}
 
-
-// function btnMenu(){
-//     if (!checkbox.checked){
-//         checkbox.setAttribute("checked", "true")
-//     }else{
-//         checkbox.removeAttribute('checked')
-//     }
-     
-// }
 
 // function controlMenu(){
 //     btnMenu()
